1730fcc7408394703c2f141b4c4ef371c145b8fc
jallow-2693 <diallomaoudia13466@gmail.com>
2025-06-06 19:48:44 +0000

    Create file.py

---
+++ backend/file.py
@@ -0,0 +1,53 @@
+import json
+import time
+import os
+from threading import Lock
+
+class FileBackend:
+    def __init__(self, filepath='braincache.json'):
+        self.filepath = filepath
+        self.lock = Lock()
+        self._load()
+
+    def _load(self):
+        if os.path.exists(self.filepath):
+            with open(self.filepath, 'r') as f:
+                try:
+                    self.cache = json.load(f)
+                except json.JSONDecodeError:
+                    self.cache = {}
+        else:
+            self.cache = {}
+
+    def _save(self):
+        with self.lock:
+            with open(self.filepath, 'w') as f:
+                json.dump(self.cache, f, indent=2)
+
+    def set(self, key, value, expire_at=None):
+        with self.lock:
+            self.cache[key] = {
+                'value': value,
+                'expire_at': expire_at
+            }
+            self._save()
+
+    def get(self, key):
+        entry = self.cache.get(key)
+        if entry:
+            if entry['expire_at'] is None or entry['expire_at'] > time.time():
+                return (entry['value'], entry['expire_at'])
+            else:
+                self.delete(key)
+        return None
+
+    def delete(self, key):
+        with self.lock:
+            if key in self.cache:
+                del self.cache[key]
+                self._save()
+
+    def clear(self):
+        with self.lock:
+            self.cache.clear()
+            self._save()
