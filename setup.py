dc0581c4d38a2762e707499053661abdf3424781
jallow-2693 <diallomaoudia13466@gmail.com>
2025-06-06 19:50:20 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="braincache",
+    version="0.1.0",
+    description="A smart, pluggable and auto-cleaning cache system for Python",
+    author="TonNom",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.7",
+)
